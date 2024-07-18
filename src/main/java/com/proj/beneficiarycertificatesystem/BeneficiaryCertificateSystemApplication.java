c1a1d0b9b6d19a0f47f525305e86b391487d26e4
kimsungyeop <skuksyzkzk@gmail.com>
2024-07-18 10:28:34 +0900

    feat: Fisrt Commit
    첫번째 커밋

---
+++ src/main/java/com/proj/beneficiarycertificatesystem/BeneficiaryCertificateSystemApplication.java
@@ -0,0 +1,13 @@
+package com.proj.beneficiarycertificatesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeneficiaryCertificateSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeneficiaryCertificateSystemApplication.class, args);
+    }
+
+}
